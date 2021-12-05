04380d6e28b86beb7dc1fe8cfd8a50d4011a8217
Athena Chang <athena830@gmail.com>
2021-12-05 13:58:29 +0800

    Add calling stored procedure function

--- src/Database.php
+++ src/Database.php
@@ -735,5 +735,72 @@ class Database
 
         return $datarow[$field];
     }
+    
+    /**
+     * close db connection
+     *
+     * @return void
+     */
+    function close()
+    {
+        $this->mysql->close();
+    }
+
+    /**
+     * Calls a Stored Procedure and returns the results as an array of rows.
+     * @param mysqli $dbLink An open mysqli object.
+     * @param string $procName The name of the procedure to call.
+     * @param string $params The parameter string to be used
+     * @return array An array of rows returned by the call.
+     */
+    function call_sp($procName, $params="")
+    {
+        // Execute the SQL command.
+        // The multy_query method is used here to get the buffered results,
+        // so they can be freeded later to avoid the out of sync error.
+        $sql = "CALL {$procName}({$params});";
+        $sqlSuccess = $this->mysql->multi_query($sql);
+
+        if($sqlSuccess)
+        {
+            if($this->mysql->more_results())
+            {
+                // Get the first buffered result set, the one with our data.
+                $result = $this->mysql->use_result();
+                $output = array();
+
+                // Put the rows into the outpu array
+                while($row = $result->fetch_assoc())
+                {
+                    $output[] = $row;
+                }
+
+                // Free the first result set.
+                // If you forget this one, you will get the "out of sync" error.
+                $result->free();
+
+                // Go through each remaining buffered result and free them as well.
+                // This removes all extra result sets returned, clearing the way
+                // for the next SQL command.
+                while($this->mysql->more_results() && $this->mysql->next_result())
+                {
+                    $extraResult = $this->mysql->use_result();
+                    if($extraResult instanceof mysqli_result){
+                        $extraResult->free();
+                    }
+                }
+
+                return $output;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            throw new Exception("The call failed: " . $this->mysql->error);
+        }
+    }
 
 }
